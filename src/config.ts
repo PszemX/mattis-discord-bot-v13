fa6163f71956c838fa89c40a27b06ac84ba52442
Przemysław Szumczyk <przemek.szumczyk@gmail.com>
2021-10-20 23:56:20 +0200

    Change the interval time

--- src/config.ts
+++ src/config.ts
@@ -4,20 +4,20 @@ import {
 	Options,
 	LimitedCollection,
 	ClientPresenceStatus,
-} from "discord.js";
+} from 'discord.js';
 
 export const discordToken =
-	process.env.ENVIRONMENT === "production"
+	process.env.ENVIRONMENT === 'production'
 		? process.env.DISCORD_PRODUCTION_TOKEN
 		: process.env.DISCORD_DEVELOPMENT_TOKEN;
 
-export const owners: string[] = JSON.parse(process.env.OWNERS ?? "[]");
+export const owners: string[] = JSON.parse(process.env.OWNERS ?? '[]');
 export const defaultPrefix: string = String(process.env.DEFAULT_PREFIX);
-export const isProd: Boolean = process.env.ENVIRONMENT === "production";
+export const isProd: Boolean = process.env.ENVIRONMENT === 'production';
 export const isDev: Boolean = !isProd;
 
 export const clientOptions: ClientOptions = {
-	allowedMentions: { parse: ["users"], repliedUser: true },
+	allowedMentions: { parse: ['users'], repliedUser: true },
 	intents: [
 		Intents.FLAGS.GUILDS,
 		Intents.FLAGS.GUILD_MESSAGES,
@@ -48,12 +48,12 @@ export const clientOptions: ClientOptions = {
 
 export const presenceData: any /*IpresenceData*/ = {
 	activities: [
-		{ name: `My default prefix is ${defaultPrefix}`, type: "PLAYING" },
-		{ name: "all questions and commands", type: "LISTENING" },
-		{ name: "the whole guild", type: "WATCHING" },
-		{ name: "Hello there, my name is M.A.T.T.I.S", type: "PLAYING" },
-		{ name: "with MEE6", type: "COMPETING" },
+		{ name: `My default prefix is ${defaultPrefix}`, type: 'PLAYING' },
+		{ name: 'all questions and commands', type: 'LISTENING' },
+		{ name: 'the whole guild', type: 'WATCHING' },
+		{ name: 'Hello there, my name is M.A.T.T.I.S', type: 'PLAYING' },
+		{ name: 'with MEE6', type: 'COMPETING' },
 	],
-	status: ["online"] as ClientPresenceStatus[],
-	interval: 60000,
+	status: ['online'] as ClientPresenceStatus[],
+	interval: 300000,
 };
