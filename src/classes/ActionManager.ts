baa566ebf95ee748a394aee0f6cd53bedd82e714
Przemysaw <przemek.szumczyk@gmail.com>
2021-10-21 16:30:14 +0200

    ActionManager class

--- src/classes/ActionManager.ts
+++ src/classes/ActionManager.ts
@@ -1,3 +1,9 @@
+import { Mattis } from './Mattis';
+
 export class ActionManager {
 	public constructor(public client: Mattis);
+
+	public load(): void {
+		this.client.logger.debug('Action loading');
+	}
 }
