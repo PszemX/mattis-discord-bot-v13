ca2ff2f2470e3c7eb90bda90b96c4091aa81a7a0
Przemysław Szumczyk <przemek.szumczyk@gmail.com>
2021-10-23 11:50:02 +0200

    Trying to make easier OOP

--- src/classes/Mattis.ts
+++ src/classes/Mattis.ts
@@ -1,32 +1,25 @@
 import { createLogger } from '../utilities/logger';
-import { EventsLoader } from './EventsLoader';
 import * as config from '../config';
-import { Client, ClientOptions } from 'discord.js';
+import { Client, ClientOptions, Collection } from 'discord.js';
 import { resolve } from 'path';
 
 export class Mattis extends Client {
 	public readonly config = config;
-
 	public readonly logger = createLogger('bot');
+
+	commands: any;
+	actions: any;
+	tasks: any;
 
 	public constructor(clientOptions: ClientOptions) {
 		super(clientOptions);
+		this.commands = 0;
+		this.actions = 0;
+		this.tasks = 0;
 	}
-
-	// public readonly Actions;
-
-	// public readonly Commands;
-
-	// public readonly Tasks;
-
-	public readonly Events = new EventsLoader(
-		this,
-		resolve(__dirname, '..', 'events')
-	);
 
 	async build(): Promise<this> {
 		const start = Date.now();
-		this.Events.load();
 		this.on('ready', async () => {
 			console.log(this);
 			this.logger.debug(`Ready took ${(Date.now() - start) / 1000}s`);
