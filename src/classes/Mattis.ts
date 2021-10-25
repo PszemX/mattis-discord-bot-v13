3703f8b4af085a2e5efdaea76bfb8f925ddf0912
Przemysaw <przemek.szumczyk@gmail.com>
2021-10-25 13:15:10 +0200

    change the name of the argument

--- src/classes/Mattis.ts
+++ src/classes/Mattis.ts
@@ -8,20 +8,19 @@ export class Mattis extends Client {
 	public readonly logger = createLogger('bot');
 
 	commands: any;
-	actions: any;
+	eventActions: any;
 	tasks: any;
 
 	public constructor(clientOptions: ClientOptions) {
 		super(clientOptions);
 		this.commands = 0;
-		this.actions = 0;
+		this.eventActions = 0;
 		this.tasks = 0;
 	}
 
 	async build(): Promise<this> {
 		const start = Date.now();
 		this.on('ready', async () => {
-			console.log(this);
 			this.logger.debug(`Ready took ${(Date.now() - start) / 1000}s`);
 		});
 		await this.login();
