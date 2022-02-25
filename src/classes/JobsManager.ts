c7c8afa6be930cb9c32800c944e4dedcdbf45f8a
Przemysaw <przemek.szumczyk@gmail.com>
2022-02-25 03:22:35 +0100

    Remove unnecessary libraries

--- src/classes/JobsManager.ts
+++ src/classes/JobsManager.ts
@@ -1,8 +1,5 @@
-import { IAction, ICommand, IEventData } from '../typings';
 import { GuildCache } from './GuildCache';
-import { Collection } from 'discord.js';
 import { Mattis } from './Mattis';
-import { BaseJob } from './BaseJob';
 
 export class JobsManager {
 	public constructor(public Mattis: Mattis, public guildCache: GuildCache) {}
