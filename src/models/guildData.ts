0cc13f6adaa2316f17c9b789804fea86fb99dda6
Przemysaw <przemek.szumczyk@gmail.com>
2022-02-26 23:21:29 +0100

    Update guildData model

--- src/models/guildData.ts
+++ src/models/guildData.ts
@@ -7,6 +7,72 @@ export const guildDataModel = (guild: Guild) => {
 		name: guild.name,
 		prefix: defaultPrefix,
 		language: baseLanguage,
-		actions: {},
+		actions: {
+			welcomeChannelMessage: {
+				enabled: false,
+				channelId: '',
+				text: '',
+				deleteTime: 0,
+			},
+			welcomePrivateMessage: {
+				enabled: false,
+				text: '',
+				deleteTime: 0,
+			},
+			welcomeRole: {
+				enabled: false,
+				roleId: '',
+			},
+			messageVerification: {
+				enabled: false,
+				messageText: '',
+				messageChannelId: '',
+				roleAddId: '',
+				roleRemoveId: '',
+			},
+			emojiVerification: {
+				enabled: false,
+				messageId: '',
+				emoji: '',
+				roleAddId: '',
+				roleRemoveId: '',
+			},
+			ping: {
+				enabled: false,
+				aliases: ['ping'],
+				cooldown: 3000,
+			},
+			covid: {
+				enabled: false,
+				aliases: ['covid'],
+				cooldown: 3000,
+			},
+			rps: {
+				enabled: false,
+				aliases: ['rps'],
+				cooldown: 3000,
+			},
+			sendMessageJob: {
+				enabled: false,
+				messageJobs: [
+					{
+						enabled: false,
+						channelId: '',
+						message: '',
+						intervalTime: 2000,
+						firstTimeSending: 0,
+						lastTimeSent: 0,
+					},
+					{
+						enabled: false,
+						channelId: '',
+						message: '',
+						intervalTime: 1000,
+						firstTimeSending: 0,
+						lastTimeSent: 0,
+					},
+				],
+			},
+		},
 	};
 };
