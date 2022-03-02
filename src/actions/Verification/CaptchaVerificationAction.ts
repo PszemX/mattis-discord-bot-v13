7b4888afe136ce5460a0a921263df57ba35ff1ac
Przemysaw <przemek.szumczyk@gmail.com>
2022-03-02 08:34:33 +0100

    Preparation of captchaVerification

--- src/actions/Verification/CaptchaVerificationAction.ts
+++ src/actions/Verification/CaptchaVerificationAction.ts
@@ -11,5 +11,20 @@ export class CaptchaVerificationAction extends BaseEventAction {
 		return true;
 	}
 
-	public async execute(EventData: IEventData) {}
+	public async execute(EventData: IEventData) {
+		const captchaCode = this.createCode();
+		await EventData.args.send(captchaCode);
+	}
+
+	private createCode(): String {
+		const numbers: string = '1234567890';
+		const lowercase: string = 'abcdefghijklmnopqrstuvwxyz';
+		const uppercase: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		const chars: string = numbers + lowercase + uppercase;
+		const code: string[] = [];
+		for (let i = 0; i < 6; i++) {
+			code.push(chars.charAt(Math.floor(Math.random() * chars.length)));
+		}
+		return code.join('');
+	}
 }
