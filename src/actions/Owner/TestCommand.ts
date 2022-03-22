40786c1968f164ebbf3e7b45721e1423dbe4511b
Przemysaw <przemek.szumczyk@gmail.com>
2022-03-22 04:40:59 +0100

    Create method to test developer things

--- src/actions/Owner/TestCommand.ts
+++ src/actions/Owner/TestCommand.ts
@@ -1,7 +1,13 @@
 import { BaseCommand } from '../../classes/BaseStructures/BaseCommand';
 import { IEventData } from '../../typings';
-import { compare } from '../../utilities/objectComparison';
 import { guildDataModel } from '../../models/guildData';
+import {
+	diff,
+	addedDiff,
+	deletedDiff,
+	updatedDiff,
+	detailedDiff,
+} from 'deep-object-diff';
 
 export class TestCommand extends BaseCommand {
 	public constructor() {
@@ -24,20 +30,44 @@ export class TestCommand extends BaseCommand {
 	public async execute(EventData: IEventData, parameters: any) {
 		if (EventData.args.author.id == '345182514492604417') {
 			const guild = EventData.args.guild;
-			const guildSettings: any = await EventData.mattis.Database.guildsData(
-				guild.id,
-				'settings'
-			).findOne({
-				id: guild.id,
-			});
-			const comparison = compare(guildSettings, guildDataModel(guild));
-			for (const key of Object.keys(comparison)) {
-				if (comparison[key].type == 'created') {
-					guildSettings[key] = comparison[key].data;
-				}
-			}
-			console.log(guildSettings);
+			const guildSettings: any = await EventData.mattis.Database.guildSettings(
+				guild.id
+			);
+			const changed = this.changedObject(guildSettings, guildDataModel(guild));
+			await EventData.mattis.Guilds.updateDatabaseGuildSettings(guild.id);
 		}
 		await EventData.args.delete();
+	}
+
+	private changedObject(objectOne: any, objectTwo: any) {
+		// Based on whole guildsData.ts object.
+		let changed: any = {};
+		for (const key in objectTwo) {
+			if (Array.isArray(objectTwo[key])) {
+				changed[key] =
+					objectOne[key] == undefined ? objectTwo[key] : objectOne[key];
+			} else if (typeof objectTwo[key] === 'object') {
+				if (objectOne[key] == undefined) {
+					changed[key] = objectTwo[key];
+				} else {
+					changed[key] = this.changedObject(objectOne[key], objectTwo[key]);
+				}
+			} else {
+				changed[key] =
+					objectOne[key] == undefined ? objectTwo[key] : objectOne[key];
+			}
+		}
+		// // Based only on new things in settings.
+		// let changed: any = objectOne;
+		// const comparison: any = addedDiff(objectOne, objectTwo);
+		// for (const key in comparison) {
+		// 	if (typeof comparison[key] === 'object') {
+		// 		if (objectOne[key] == undefined) objectOne[key] = {};
+		// 		changed[key] = this.changedObject(objectOne[key], objectTwo[key]);
+		// 	} else {
+		// 		changed[key] = comparison[key];
+		// 	}
+		// }
+		return changed;
 	}
 }
