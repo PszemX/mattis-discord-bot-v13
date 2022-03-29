cef5ad1a0c67f4c1e1d809031243b7622fd542b8
Przemysaw <przemek.szumczyk@gmail.com>
2022-03-29 03:27:32 +0200

    Refactor EmojiProtectionAction

--- src/actions/Automod/EmojiProtectionAction.ts
+++ src/actions/Automod/EmojiProtectionAction.ts
@@ -8,9 +8,7 @@ export class EmojiProtectionAction extends BaseEventAction {
 
 	public async trigger(EventData: IEventData) {
 		const settings = EventData.guildCache.settings.actions[this.name];
-		const message = EventData.args.content
-			.replace(/ /g, '')
-			.replace(/<a?:[a-z0-9_]+:\d+>|:[a-z0-9_]+:/gi, '😀');
+		const message = EventData.args.content.replace(/<a?:[a-z0-9_]+:\d+>|:[a-z0-9_]+:/gi, '😀').replace(/ /g, '');
 		if (message.length <= settings.minMessageLength) return false;
 		const emojiRegex =
 			/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*/gi;
