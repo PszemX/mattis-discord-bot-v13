5770e0706f41e813660fd40b972f6bf7635c7d65
Przemysaw <przemek.szumczyk@gmail.com>
2022-03-30 23:11:36 +0200

    Prepare channelsChanging action

---
+++ src/actions/Automod/ChannelsChangingProtectionAction.ts
@@ -0,0 +1,31 @@
+import { BaseEventAction } from '../../classes/BaseStructures/BaseEventAction';
+import { IEventData } from '../../typings';
+
+export class ChannelsChangingProtectionAction extends BaseEventAction {
+	public constructor() {
+		super('channelsChangingProtection', 'voiceStateUpdate');
+	}
+
+	public async trigger(EventData: IEventData) {
+		if (
+			!EventData.args[1].channelId ||
+			EventData.args[0].channelId == EventData.args[1].channelId
+		)
+			return false;
+		const settings = EventData.guildCache.settings.actions[this.name];
+		return (
+			EventData.guildCache.cacheManager
+				.getMemberCache(EventData.args.member)
+				.channelsChangingProtection.filter(
+					(voiceChannelJoin: any) =>
+						Date.now() - voiceChannelJoin.time < settings.perMilisecondsTime
+				).length > settings.maxChannels
+		);
+	}
+
+	public async execute(EventData: IEventData) {
+		const settings = EventData.guildCache.settings.actions[this.name];
+		EventData.args.member.voice.disconnect();
+		// Kara
+	}
+}
