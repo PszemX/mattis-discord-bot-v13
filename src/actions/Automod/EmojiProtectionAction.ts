38533d48d3930e396af53af6c0efd86fcda1d8ef
Przemysław Szumczyk <przemek.szumczyk@gmail.com>
2022-03-30 15:31:42 +0200

    Working emojisProtection

--- src/actions/Automod/EmojiProtectionAction.ts
+++ src/actions/Automod/EmojiProtectionAction.ts
@@ -8,11 +8,13 @@ export class EmojiProtectionAction extends BaseEventAction {
 
 	public async trigger(EventData: IEventData) {
 		const settings = EventData.guildCache.settings.actions[this.name];
-		const message = EventData.args.content.replace(/(<a?)?:.+?:(\d{18}>)?/gi, '️♥').replace(/️+/g, '');
-		if (message.length <= settings.minMessageLength) return false;
+		const outsideEmojiRegex = /(<a?)?:.+?:(\d{18}>)?/gi;
 		const emojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/gi;
-		const newmsg = message.replace(emojiRegex, '');
-		return (newmsg.length / message.length) * 100 < settings.maxPercentage;
+		const message = EventData.args.toString().replace(outsideEmojiRegex, '️♥').replace(emojiRegex, '♥').replace(/️+/g, '');
+		if (message.length <= settings.minMessageLength) return false;
+		const messageWithoutEmojis = message.replace(/♥/g, '');
+		const emojisAmout = message.length - messageWithoutEmojis.length;
+		return (emojisAmout / message.length) * 100 > settings.maxPercentage;
 	}
 
 	public async execute(EventData: IEventData) {
