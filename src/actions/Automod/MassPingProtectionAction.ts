34bebdf239c99dba1ed8bed8886036371018fe9b
Przemysaw <przemek.szumczyk@gmail.com>
2022-04-03 23:27:53 +0200

    Create files to extend

---
+++ src/actions/Automod/MassPingProtectionAction.ts
@@ -0,0 +1,18 @@
+import { BaseEventAction } from '../../classes/BaseStructures/BaseEventAction';
+import { IEventData } from '../../typings';
+
+export class MassPingProtectionAction extends BaseEventAction {
+	public constructor() {
+		super('massPingProtection', 'messageCreate');
+	}
+
+	public async trigger(EventData: IEventData) {
+		return false;
+	}
+
+	public async execute(EventData: IEventData) {
+		const settings = EventData.guildCache.settings.actions[this.name];
+		EventData.args.delete();
+		// Kara
+	}
+}
