6c3965cbcc69e329f4f0f85a060ecc7f8aed6b8b
Przemysław Szumczyk <przemek.szumczyk@gmail.com>
2022-04-11 13:51:17 +0200

    cache voice state updates

--- src/events/VoiceStateUpdateEvent.ts
+++ src/events/VoiceStateUpdateEvent.ts
@@ -1,8 +1,16 @@
+import { VoiceState } from 'discord.js';
 import { BaseEvent } from '../classes/BaseStructures/BaseEvent';
+import { IEventData } from '../typings';
 
 export class VoiceStateUpdateEvent extends BaseEvent {
 	public constructor(mattis: BaseEvent['mattis']) {
 		super(mattis, 'voiceStateUpdate');
 	}
-	public async execute(): Promise<void> {}
+	public async execute(oldState: VoiceState, newState: VoiceState): Promise<void> {
+		const EventData: IEventData = this.mattis.utils.getEventData(oldState, newState);
+		const { member } = newState;
+		const timestamp = Date.now();
+		const hashedCacheData = `${member}.${oldState.channelId}.${newState.channelId}.${timestamp}`;
+		EventData.guildCache.cacheManager.getMemberCache(member).voiceStates.push(hashedCacheData);
+	}
 }
