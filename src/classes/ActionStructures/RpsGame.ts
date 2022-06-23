a7a6615c4fe622f3193a204de36fa22c0c75a13e
Przemysaw <przemek.szumczyk@gmail.com>
2022-06-24 01:55:07 +0200

    Make better if

--- src/classes/ActionStructures/RpsGame.ts
+++ src/classes/ActionStructures/RpsGame.ts
@@ -63,13 +63,14 @@ export class RpsGame extends BaseClient {
 		if (this.playerTwo?.id != clientId) {
 			this.playersCount = 2;
 			this.isMultiplayerGame = true;
+			console.log('MULTI');
 		}
 		await this.gameBegin();
 	}
 
 	private async gameBegin() {
 		if (!this.attachment) await this.createMessageAttachment();
-		if (this.isMultiplayerGame) {
+		if (this.isMultiplayerGame === true) {
 			await this.inviteOpponent();
 		} else {
 			await this.generateGameBeginMessage();
